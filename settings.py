210f530db6a21c8301bac99882ab412a738f8ec7
marcelbra <marcelbraasch@gmail.com>
2024-05-16 23:59:51 +0200

    Dir change

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
-OUTPUT_DIR = "/cluster/work/lawecon/Work/mbraasch/projects/moe_decomposition/output"
-DATA_DIR = "/cluster/work/lawecon/Work/mbraasch/projects/moe_decomposition/data"
+OUTPUT_DIR = "/cluster/work/lawecon/Work/mbraasch/output"
+DATA_DIR = "/cluster/work/lawecon/Work/mbraasch/data"
 TRAIN_METRICS_FILE_NAME = "metrics.json"
 TRAINING_ARGS_FILE_NAME = "training_args.json"
 GENERATION_CONFIG_FILE_NAME = "generation_args.json"
