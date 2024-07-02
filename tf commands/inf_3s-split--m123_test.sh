44cb5dc55287efe7d9a8455896f87b41cee1c2b3
marcelbra <marcelbraasch@gmail.com>
2024-07-02 16:24:16 +0200

    Remove commands

--- tf commands/inf_3s-split--m123_test.sh
+++
@@ -1,6 +0,0 @@
-sbatch --gpus=rtx_3090:1 --mem-per-cpu=8G --time=00:05:00 --wrap="python3 sft/inference.py \
-       --run_name gsm8k-gt/3s-overlap-split-tf/m123 \
-       --postfix _test"
-sbatch --gpus=rtx_3090:1 --mem-per-cpu=8G --time=00:05:00 --wrap="python3 sft/inference.py \
-       --run_name gsm8k-dl/3s-overlap-split-tf/m123 \
-       --postfix _test"
