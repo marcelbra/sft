1c89b1aef8d395149f9d9e115145622a1b8f0461
marcelbra <marcelbraasch@gmail.com>
2024-07-18 01:01:43 +0200

    update inference script

--- inference.py
+++ inference.py
@@ -23,10 +23,6 @@ def get_max_step_adapter_name(output_dir):
     return os.path.join(output_dir, f"{ckpt_name}{max_step}", "adapter_model")
 
 
-def format_question(question: str, formatting_template: str):
-    return formatting_template.format(question.strip()).lstrip()
-
-
 def initialize_engine(model_name_or_path) -> LLMEngine:
     print("Initialize the LLMEngine.")
     kwargs = {}
@@ -64,9 +60,10 @@ def initialize_engine(model_name_or_path) -> LLMEngine:
 def get_sampling_params(eot_token):
     print("Initalizating sampling params.")
     return SamplingParams(
-        temperature=0,
+        n = 10,
+        temperature=0.7,
         max_tokens=2048,
-        stop=[eot_token]
+        stop=["\n"]
     )
 
 
@@ -76,132 +73,82 @@ def get_lora_request(lora_path: str):
     print(f"Loaded ckpt {adapter_ckpt}.")
     return LoRARequest("lora", 1, adapter_ckpt)
 
-def split_steps(next_steps: List[dict]):
-
-    same = 0
-    different = 0
-    error_count = 0
-    strategy_one = [] # Here, we naively split off the first step
-    strategy_two = [] # Here, we only split if the first step has a "=", if not we go on until the next and remove the "\n"s in between
-
-    for element in next_steps:
-
-        steps = [x for x in element["prediction"].split("\n") if x]
-        if not steps:
-            error_count += 1
-            print("Error:")
-            print(element)
-            continue
-
-        # strategy one
-        # Add "." if the step does not end with a "." or with a ":"
-        if not steps[0].endswith(".") and not steps[0].endswith(":"):
-            steps[0] += "."
-        new_element_s1 = {
-            "instruction": element["instruction"],
-            "prediction": steps[0]
-        }
-        strategy_one.append(new_element_s1)
-
-        # strategy two
-        new_element_s2 = {
-            "instruction": element["instruction"],
-            "prediction": ""
-        }
-        new_steps = ""
-        for step in steps:
-            if "=" in step:
-                new_steps += step + (". " if not step.endswith(".") or step.endswith(":") else "")
-                break
-            else:
-                if not step.endswith(".") and not step.endswith(":"):
-                    new_steps += step + ". "
-                elif step.endswith(".") or step.endswith(":"):
-                    new_steps += step + " "
-        new_element_s2["prediction"] = new_steps.strip()
-        new_steps = new_steps.strip()
-        if not new_steps.endswith(".") and not new_steps.endswith(":"):
-            new_steps += "."
-        new_element_s2["prediction"] = new_steps.strip()
-        strategy_two.append(new_element_s2)
-    
-        # Statistics on how much overlap there is
-        if new_element_s1["prediction"] == new_element_s2["prediction"]:
-            same += 1
-        else:
-            different += 1
-
-    print(f"Overlap in percentage: {same / (same + different) * 100}%")
-    print(f"Error count: {error_count}")
-
-    return {"s1": strategy_one, "s2": strategy_two}
-
 
 def write_results(run_name, next_steps, final_result, previous_next_steps, postfix):
 
-        strategy = ""
-        if previous_next_steps:
-            if "s1" in previous_next_steps:
-                strategy = "s1" 
-                print(f"Strategy specified: {strategy}")
-            elif "s2" in previous_next_steps:
-                strategy = "s2"
-                print(f"Strategy specified: {strategy}")
-        else:
-            print("No strategy specified.")
-
-        # First, write the next step predictions
-        file_name = f"{strategy}_next_step_predictions{postfix}.json" if strategy else f"next_step_predictions{postfix}.json"
+        file_name = f"next_step_predictions{postfix}.json"
         next_step_path = os.path.join(run_name, file_name)
         print(f"Writing {next_step_path}.")
         with open(next_step_path, "w") as f:
             json.dump(next_steps, f, indent=4, ensure_ascii=False)
 
         # Then, write the final results
-        file_name = f"{strategy}_final_results{postfix}.json" if strategy else f"final_results{postfix}.json"
+        # file_name = f"{strategy}_final_results{postfix}.json" if strategy else f"final_results{postfix}.json"
+        file_name = f"final_results{postfix}.json"
         final_results_path = os.path.join(run_name, file_name)
         print(f"Writing {final_results_path}.")
         with open(final_results_path, "w") as f:
             json.dump(final_result, f, indent=4, ensure_ascii=False)
 
-        # Lastly, split the data and write it
-        if strategy:
-            split = split_steps(next_steps)
-            split_next_steps = os.path.join(run_name, f"{strategy}_next_step_predictions_{strategy}{postfix}.json")
-            final_split = split["s1"] if "s1" in split_next_steps else split["s2"]
-            print(f"Writing {split_next_steps}.")
-            with open(split_next_steps, "w") as f:
-                json.dump(final_split, f, indent=4, ensure_ascii=False)
-            # else:
-            # split_next_steps_s1 = os.path.join(run_name, f"s1_next_step_predictions_s1{postfix}.json")
-            # print(f"Writing {split_next_steps_s1}.")
-            # with open(split_next_steps_s1, "w") as f:
-            #     json.dump(split["s1"], f, indent=4, ensure_ascii=False)
-            # split_next_steps_s2 = os.path.join(run_name, f"s2_next_step_predictions_s2{postfix}.json")
-            # print(f"Writing {split_next_steps_s2}.")
-            # with open(split_next_steps_s2, "w") as f:
-            #     json.dump(split["s2"], f, indent=4, ensure_ascii=False)
 
-
-def format_data(data_dir, sampling_params, lora_request, previous_next_steps=None, amount_samples=None):
+def format_data(data_dir, sampling_params, lora_request, previous_next_steps=None, amount_samples=None, previous_step_n = 0):
             
     if previous_next_steps:
+        
+        # print(f"Load data from {previous_next_steps}")
+        # with open(previous_next_steps) as f:
+        #     data = json.load(f)
+        # formatted_data = []
+        # for element in data:
+        #     if "Final answer" in element["prediction"]:
+        #         continue
+        #     question, steps = element["instruction"].split("\n\n### Input:\n")[1].split("\n\n### Response:\n")
+        #     maybe_linebreak = "\n" if not steps.endswith("\n") else ""
+        #     formatted_data.append(
+        #         {
+        #             "instruction": build_source_prompt(
+        #                 question=question,
+        #                 steps=steps + maybe_linebreak + element["prediction"]
+        #             ),
+        #             "sampling_params": sampling_params,
+        #             "lora_request": lora_request
+        #         }
+        #     )
 
         print(f"Load data from {previous_next_steps}")
         with open(previous_next_steps) as f:
             data = json.load(f)
+        
         formatted_data = []
         for element in data:
-            if "Final answer" in element["prediction"]:
+            
+            # steps = element["prediction"].count("\n")
+            # print("steps")
+            # print(steps)
+            # print("previous_step_n")
+            # print(previous_step_n)
+            if steps <= previous_step_n:
                 continue
-            question, steps = element["instruction"].split("\n\n### Input:\n")[1].split("\n\n### Response:\n")
-            maybe_linebreak = "\n" if not steps.endswith("\n") else ""
+            
+            question = element["instruction"].split("\n\n### Input:\nQuestion: ")[1].split("\n\n### Response:")[0].split("\n<step ")[0]
+            step = element["prediction"].split("\n")[previous_step_n-1].replace(f"<step {previous_step_n}>: ", "")
+            instruction = build_source_prompt(
+                question=question,
+                steps=step,
+                previous_step_n=previous_step_n
+            )
+
+            # print("Question:")
+            # print(question)
+            # print("Step:")
+            # print(step)
+            print("Source prompt:")
+            print(instruction)
+            print("-----------------")
+
             formatted_data.append(
                 {
-                    "instruction": build_source_prompt(
-                        question=question,
-                        steps=steps + maybe_linebreak + element["prediction"]
-                    ),
+                    "instruction": instruction,
                     "sampling_params": sampling_params,
                     "lora_request": lora_request
                 }
@@ -295,7 +242,8 @@ def run_inference(
     previous_next_steps: str = None,
     last_step: bool = False,
     postfix: str = "",
-    eos_token: str = "<eos>"
+    eos_token: str = "<eos>",
+    previous_step_n: int = 0
 ):  
     if previous_next_steps:
         print(f"Previous next steps given: {previous_next_steps}")
@@ -308,8 +256,10 @@ def run_inference(
         data_dir=data_dir,
         sampling_params=sampling_params,
         lora_request=lora_request,
-        previous_next_steps=previous_next_steps
+        previous_next_steps=previous_next_steps,
+        previous_step_n=previous_step_n
     )
+    print(data)
     engine = initialize_engine(model_name_or_path)
     results = process_requests(engine, data)
     next_steps, done = filter_out_results(results)
@@ -327,6 +277,7 @@ if __name__ == "__main__":
     parser.add_argument("--model_name_or_path", type=str, required=True)
     parser.add_argument("--previous_next_steps", type=str, default=None)
     parser.add_argument("--postfix", type=str, default="")
+    parser.add_argument("--previous_step_n", type=int)
     parser.add_argument("--data_dir", type=str, default="/cluster/work/lawecon/Work/mbraasch/data/gsm8k_test.json")
     args = parser.parse_args()
 
@@ -340,4 +291,5 @@ if __name__ == "__main__":
         postfix=args.postfix,
         eos_token=AutoTokenizer.from_pretrained(args.model_name_or_path, **kwargs).eos_token,
         data_dir=args.data_dir,
+        previous_step_n=args.previous_step_n
     )
